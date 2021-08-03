0e18ea7dc98d3a918e18a64a884b6ef9b653d6f1
Koba <koba@dummy.com>
2021-08-03 17:42:25 +0900

    z

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject m_rectPrefab = null;
+
+    List<RectsController> m_rects;
+
+    const float kX0 = -12;
+    const float kY0 = -5;
+    const float kX1 = 12;
+    const float kY1 = 5;
+
+
     void Start()
     {
-        
+        this.m_rects = new List<RectsController>();
+
+        const int kRectsNum = 70;
+        for (int i = 0; i < kRectsNum; ++i)
+        {
+            // 矩形の初期位置を決める
+            Vector2 pos = new Vector2();
+            Vector2 size = new Vector2();
+            {
+                int counter = 0;
+                const int kCounterTh = 1000;
+
+                while (counter < kCounterTh)
+                {
+                    size.x = Random.Range(0.1f, 0.5f);
+                    size.y = Random.Range(0.1f, 0.5f);
+                    pos.x = Random.Range(kX0, kX1 - size.x);
+                    pos.y = Random.Range(kY0, kY1 - size.y);
+
+                    bool hit = this.CheckHit(pos.x, pos.y, size.x, size.y, -1);
+                    if (!hit)
+                    {
+                        break;
+                    }
+
+                    ++counter;
+                }
+                if (counter >= kCounterTh)
+                {
+                    break;
+                }
+            }
+
+            // インスタンス化、初期化
+            GameObject go = Instantiate(m_rectPrefab);
+            RectsController rc = go.GetComponent<RectsController>();
+            rc.Init(this, pos, size, m_rects.Count);
+            m_rects.Add(rc);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckHit(float x, float y, float w, float h, int ignore_index)
     {
-        
+        // 画面端との接触チェック
+        if (x < kX0 || x + w > kX1 ||
+            y < kY0 || y + h > kY1)
+        {
+            return true;
+        }
+
+        // 個々の矩形との接触チェック
+        for (int i = 0; i < this.m_rects.Count; ++i)
+        {
+            if (i == ignore_index)
+            {
+                continue;
+            }
+
+            bool hit = m_rects[i].CheckHit(x, y, w, h);
+            if (hit)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/RectsController.cs
+++ Assets/Scripts/RectsController.cs
@@ -4,15 +4,130 @@ using UnityEngine;
 
 public class RectsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameController m_gc;
+    Vector2 m_size;
+    int m_dir;
+    float m_vel;
+    int m_index;
+
+    // 後続する矩形
+    [SerializeField]
+    GameObject m_followingRectPrefab = null;
+    const int kFRN = 32;
+    GameObject[] m_frs = new GameObject[kFRN];
+
+
+    public void Init(GameController gc, Vector2 pos, Vector2 size, int index)
     {
-        
+        this.m_gc = gc;
+        this.m_size = size;
+        this.m_dir = (int)Random.Range(0, 4);
+        this.m_vel = Random.Range(0.01f, 0.1f);
+        this.m_index = index;
+
+        Vector2 centerPos = new Vector2(pos.x + size.x / 2, pos.y + size.y / 2);
+        this.transform.position = centerPos;
+        this.transform.localScale = this.m_size;
+        Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        this.GetComponent<SpriteRenderer>().color = color;
+
+        // 後続する矩形を生成
+        for (int i = 0; i < kFRN; ++i)
+        {
+            GameObject go = Instantiate(this.m_followingRectPrefab);
+            this.m_frs[i] = go;
+
+            go.GetComponent<Transform>().position = centerPos;
+            go.GetComponent<Transform>().localScale = this.m_size;
+            go.GetComponent<SpriteRenderer>().color = color;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Move(float vel)
     {
-        
+        Vector3 pos = this.transform.position;
+
+        switch (this.m_dir)
+        {
+            case 0:
+                pos.y += vel;
+                break;
+            case 1:
+                pos.x += vel;
+                break;
+            case 2:
+                pos.y -= vel;
+                break;
+            case 3:
+                pos.x -= vel;
+                break;
+            default:
+                throw new System.Exception();
+        }
+
+        this.transform.position = pos;
+    }
+
+    void FixedUpdate()
+    {
+        // 先頭の矩形を前進
+        this.Move(this.m_vel);
+
+        // 衝突したら後退して方向転換
+        Vector2 centerPos = this.transform.position;
+        Vector2 size = this.m_size;
+        Vector2 pos = new Vector2(centerPos.x - size.x / 2, centerPos.y - size.y / 2);
+        bool hit = this.m_gc.CheckHit(pos.x, pos.y, size.x, size.y, this.m_index);
+        if (hit)
+        {
+            this.Move(-this.m_vel);
+            this.m_dir = (this.m_dir + 1) % 4;
+        }
+
+        // 後続も前進
+        for (int i = kFRN - 1; i > 0; --i)
+        {
+            this.m_frs[i].transform.position = this.m_frs[i - 1].transform.position;
+        }
+        if (kFRN > 0)
+        {
+            this.m_frs[0].transform.position = this.transform.position;
+        }
+    }
+
+    public bool CheckHit(float x, float y, float w, float h)
+    {
+        // 先頭
+        {
+            Vector2 centerPos = this.transform.position;
+            Vector2 size = this.m_size;
+            Vector2 pos = new Vector2(centerPos.x - size.x / 2, centerPos.y - size.y / 2);
+
+            if (pos.x + size.x / 2 >= x &&
+                pos.x <= x + w &&
+                pos.y + size.y / 2 >= y &&
+                pos.y <= y + h)
+            {
+                return true;
+            }
+        }
+
+            // 後続
+            for (int i = 0; i < kFRN; ++i)
+        {
+            Vector2 centerPos = this.m_frs[i].transform.position;
+            Vector2 size = this.m_size;
+            Vector2 pos = new Vector2(centerPos.x - size.x / 2, centerPos.y - size.y / 2);
+
+            if (pos.x + size.x / 2 >= x     &&
+                pos.x              <= x + w &&
+                pos.y + size.y / 2 >= y     &&
+                pos.y              <= y + h )
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
