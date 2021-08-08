28f70b06e5f3e850982fe6e412fde80f621a0ecf
Koba <koba@dummy.com>
2021-08-08 23:10:59 +0900

    不要なコードを削除

--- Assets/AlphaGaussian/Scripts/AlphaGaussianController.cs
+++ Assets/AlphaGaussian/Scripts/AlphaGaussianController.cs
@@ -116,14 +116,12 @@ public class AlphaGaussianController : MonoBehaviour
             {
                 ball.pos.x -= ball.vel.x;
                 ball.vel.x *= -1;
-                //ball.pos.x += ball.vel.x;
             }
             if (ball.pos.y - ball.r < 0 ||
                 ball.pos.y + ball.r >= this.m_tex.height)
             {
                 ball.pos.y -= ball.vel.y;
                 ball.vel.y *= -1;
-                //ball.pos.y += ball.vel.y;
             }
 
             // 重力
