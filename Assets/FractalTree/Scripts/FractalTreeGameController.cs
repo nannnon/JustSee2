07f9772ba7fca2bbe10f156b378391238d5d0378
Koba <koba@dummy.com>
2021-08-13 22:48:36 +0900

    設置

---
+++ Assets/FractalTree/Scripts/FractalTreeGameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalTreeGameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_fractalTreePrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
